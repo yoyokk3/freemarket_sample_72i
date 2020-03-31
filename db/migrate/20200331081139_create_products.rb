e876c8f9fb8ca05a65922073dcf05c4a24f03758
yoyokk3 <katou0503you74@gmail.com>
2020-03-31 18:04:45 +0900

    productテーブルの作成
    カラムは商品名、値段、状態、説明、送料、発送までの日、user_id

--- db/migrate/20200331081139_create_products.rb
+++ db/migrate/20200331081139_create_products.rb
@@ -1,11 +1,13 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string :name
-      t.integer :price
-      t.string :status
-      t.text :description
-      t.integer :sending
+      t.string :name, null: false
+      t.integer :price, null: false
+      t.string :status, null: false
+      t.text :description, null: false
+      t.integer :sending, null: false
+      t.integer :sendCost, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
