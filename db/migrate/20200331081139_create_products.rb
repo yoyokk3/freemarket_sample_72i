be1f79ab18d2d0ce89ba783d3ddeacd3401544f3
yoyokk3 <katou0503you74@gmail.com>
2020-03-31 19:36:43 +0900

    user, product, addressの文法エラーを治した

--- db/migrate/20200331081139_create_products.rb
+++ db/migrate/20200331081139_create_products.rb
@@ -1,13 +1,13 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string     :name         null: false
-      t.integer    :price        null: false
-      t.string     :status       null: false
-      t.text       :description  null: false
-      t.integer    :sending      null: false
-      t.integer    :sendCost     null: false
-      t.references :user         null: false, foreign_key: true
+      t.string     :name,         null: false
+      t.integer    :price,        null: false
+      t.string     :status,       null: false
+      t.text       :description,  null: false
+      t.integer    :sending,      null: false
+      t.integer    :sendCost,     null: false
+      t.references :user,         null: false, foreign_key: true
       t.timestamps
     end
   end
