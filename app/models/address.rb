bd01fd57a8d9a58f7383adae0fe562dc7eb9f555
yoyokk3 <katou0503you74@gmail.com>
2020-04-01 18:43:11 +0900

    全てのモデル、カラムを作りアソシエーションを組んだ

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,4 @@
 class Address < ApplicationRecord
-
  belongs_to :user
- has_one    :product
+ belongs_to :product
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,9 @@
 class Product < ApplicationRecord
 
-  enum status:[:brand_new, :very_good, :good]
-
  belongs_to :user
  belongs_to :category
  belongs_to :brand
- belongs_to :address
+ has_one    :address
  has_many   :likes
  has_many   :images
 end
