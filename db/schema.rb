abe0dd24d857b3c78ce0f44a79bf8d4724a2929b
yoyokk3 <katou0503you74@gmail.com>
2020-04-01 13:04:39 +0900

    Revert "likeテーブルを作った"
    This reverts commit 662034c1973c9c8f545a9696cb7b4e862f813724.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_103839) do
+ActiveRecord::Schema.define(version: 2020_03_31_091932) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
@@ -27,15 +27,6 @@ ActiveRecord::Schema.define(version: 2020_03_31_103839) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
-  end
-
-  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "product_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_likes_on_product_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -64,7 +55,5 @@ ActiveRecord::Schema.define(version: 2020_03_31_103839) do
   end
 
   add_foreign_key "addresses", "users"
-  add_foreign_key "likes", "products"
-  add_foreign_key "likes", "users"
   add_foreign_key "products", "users"
 end
