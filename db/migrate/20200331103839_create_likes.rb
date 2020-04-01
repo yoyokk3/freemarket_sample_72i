972654435b734f339060d2e6a79f9856ad06071a
yoyokk3 <katou0503you74@gmail.com>
2020-04-01 13:04:39 +0900

    Revert "likeテーブルを作った"
    This reverts commit 662034c1973c9c8f545a9696cb7b4e862f813724.

--- db/migrate/20200331103839_create_likes.rb
+++
@@ -1,9 +0,0 @@
-class CreateLikes < ActiveRecord::Migration[5.2]
-  def change
-    create_table :likes do |t|
-      t.references :user,      null: false, foreign_key: true
-      t.references :product,   null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
