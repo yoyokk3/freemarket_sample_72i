fe37d86e729ac079aee806a867bd77db0aa08b98
yoyokk3 <katou0503you74@gmail.com>
2020-04-01 13:15:28 +0900

    Revert "Revert "addressテーブルの作成""
    This reverts commit 2c93ba055af992d5e9752c6ae378533e78a2dfd1.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_081139) do
+ActiveRecord::Schema.define(version: 2020_03_31_091932) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_reading", null: false
+    t.string "last_name_reading", null: false
+    t.integer "postal_code", null: false
+    t.string "to_do_fu_ken", null: false
+    t.string "si_ku_cho_son", null: false
+    t.string "banti", null: false
+    t.string "apartment_name"
+    t.integer "call_number", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -37,5 +54,6 @@ ActiveRecord::Schema.define(version: 2020_03_31_081139) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
   add_foreign_key "products", "users"
 end
