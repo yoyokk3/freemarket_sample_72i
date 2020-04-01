fad80939cc62ace4509c85ce804073b6e8a176ec
yoyokk3 <katou0503you74@gmail.com>
2020-04-01 13:17:25 +0900

    Revert "Revert "cardテーブルを作った""
    This reverts commit 95475744c62482ce9e2adfe391e1d14b6951c9b4.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_103839) do
+ActiveRecord::Schema.define(version: 2020_03_31_104030) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
@@ -27,6 +27,19 @@ ActiveRecord::Schema.define(version: 2020_03_31_103839) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "card_number", null: false
+    t.string "brand", null: false
+    t.integer "deadline_year", null: false
+    t.integer "deadline_manth", null: false
+    t.string "name_onCard", null: false
+    t.integer "security_code", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -64,6 +77,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_103839) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "cards", "users"
   add_foreign_key "likes", "products"
   add_foreign_key "likes", "users"
   add_foreign_key "products", "users"
