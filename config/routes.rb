35bd9abc770f16c85ef801b52e2d5694daf2ab57
shimpei1009 <gogo.ikeike.1009@icloud.com>
2020-04-02 21:48:55 +0900

    Revert "Merge pull request #25 from yoyokk3/Exhibition-btn"
    This reverts commit 7445831c9b72323e1a78d29f255e8b2ea9d524ac, reversing
    changes made to d869f9717ae85835a90f9f80629976d49600e841.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'users/show'
-  get "users/logout"
   devise_for :users
   root to: 'tops#index'
   resources :tops
-  resources :users
 end
--- db/migrate/20200401093407_create_images.rb
+++ db/migrate/20200401093407_create_images.rb
@@ -2,6 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
       t.text          :image,   null: false
+      t.references    :user,    null: false, foreign_key: true
       t.references    :product, null: false, foreign_key: true
       t.timestamps
     end
