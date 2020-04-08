4a6573cb48f191278395bb76a687eaabac8a09fe
harunon78 <noon.spy-luv95@softbank.ne.jp>
2020-04-08 15:46:41 +0900

    バリデーション変更

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,13 @@
 class Address < ApplicationRecord
-  belongs_to :user
-  belongs_to :product
+  belongs_to :user, optional: true
+
+  validates :first_name, presence: true ,format: { with: /\A[一-龥ぁ-ん]/}
+  validates :last_name, presence: true ,format: { with: /\A[一-龥ぁ-ん]/ }
+  validates :first_name_kana, presence: true ,format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana, presence: true ,format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :postal_code, presence: true
+  validates :prefectures, presence: true
+  validates :municipality, presence: true
+  validates :house_number, presence: true
+  
 end
